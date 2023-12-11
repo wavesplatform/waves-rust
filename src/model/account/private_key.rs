87cea8bcf7fe63ef01f56d29c4b1bf91d1eb2074
Artem Badrtdinov <bodrych@gmail.com>
2023-12-11 17:45:35 +0500

    VectorToArrayConversionError

--- src/model/account/private_key.rs
+++ src/model/account/private_key.rs
@@ -17,10 +17,7 @@ impl std::str::FromStr for PrivateKey {
 
     fn from_str(base58string: &str) -> Result<PrivateKey> {
         let bytes = Base58::decode(base58string)?;
-        let bytes_array: [u8; 32] = match bytes.try_into() {
-            Ok(v) => v,
-            Err(_) => return Err(Error::PrivateKeyConversionError),
-        };
+        let bytes_array: [u8; 32] = bytes.try_into().map_err(|e| Error::VectorToArrayConversionError(e))?;
         PrivateKey::from_bytes(bytes_array)
     }
 }
